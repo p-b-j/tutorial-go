fed779d509e4bb35ec3a44f0ff8bc6bd7d5028f7
Porter Jones <porter@pbj.local>
2022-02-01 21:15:38 -0800

    remove fuzz test

--- method_test.go
+++ method_test.go
@@ -9,9 +9,3 @@ func FuzzBrokenMethod(f *testing.F) {
     BrokenMethod(str)
   })
 }
-
-func FuzzBrokenMethodNoSeed(f *testing.F) {
-  f.Fuzz(func(t *testing.T, str string) {
-    BrokenMethod(str)
-  })
-}
