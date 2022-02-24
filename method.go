17fd667ef1e749b66d1df50e5204b25da0dbfc15
Porter Jones <porter@fuzzbuzz.io>
2022-02-24 13:10:53 -0800

    Revert "Fix bug"
    This reverts commit 01d2005c5d2a8dbedb0b12deca1618563572c623.

--- method.go
+++ method.go
@@ -5,8 +5,9 @@ package tutorial
 func BrokenMethod(Data string) bool {
 	return len(Data) >= 3 &&
 		Data[0] == 'F' &&
-		Data[1] == 'I' &&
-		Data[2] == 'Z'
+		Data[1] == 'O' &&
+		Data[2] == 'U' &&
+		Data[3] == 'R'
 }
 
 
