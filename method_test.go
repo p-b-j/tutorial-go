bc5d29bf7ad565a8dba5b594f389e56d588e3b9e
Porter Jones <porter@fuzzbuzz.io>
2022-03-07 11:17:35 -0800

    Removing test

--- method_test.go
+++ method_test.go
@@ -10,13 +10,6 @@ func FuzzBrokenMethod(f *testing.F) {
   })
 }
 
-func FuzzBrokenMethodNoSeed(f *testing.F) {
-  f.Add("F")
-  f.Fuzz(func(t *testing.T, str string) {
-    BrokenMethod(str)
-  })
-}
-
 func FuzzTestNumberInputMethod(f *testing.F) {
   f.Fuzz(func(t *testing.T, data1 bool, data2 int, data3 string) {
     NumberInputMethod(data1, data2, data3)
