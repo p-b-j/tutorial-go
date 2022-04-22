04fba07a03ee0e4373d310094355021f2a25dd33
Porter Jones <porter@fuzzbuzz.io>
2022-04-22 10:09:50 -0700

    Add a bunch of bugs

--- method.go
+++ method.go
@@ -6,7 +6,8 @@ func BrokenMethod(Data string) bool {
 	return len(Data) >= 3 &&
 		Data[0] == 'F' &&
 		Data[1] == 'O' &&
-		Data[2] == 'U'
+		Data[2] == 'U' &&
+		Data[3] == 'R'
 }
 
 
@@ -15,12 +16,20 @@ func NumberInputMethod(Data1 bool, Data2 int, Data3 string) int {
         return 1
     }
 
-    if Data2 > 0 {
-        return 1
+    if Data2 == 0 {
+        return 5 / Data2
+    }
+
+    if Data2 == 1 {
+        return 5 / (Data2 - 1)
+    }
+
+    if len(Data3) == 1 {
+        return int(Data3[1])
     }
 
     if len(Data3) == 0 {
-        return 1
+        return int(Data3[0])
     }
 
     return 0
