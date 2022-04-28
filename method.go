c662928c2bfa89b9a95127b982846041281ccbad
Porter Jones <porter@fuzzbuzz.io>
2022-04-28 13:01:42 -0700

    Revert "break build"
    This reverts commit eff9272346d0c04c02fbcac511fbec365cae6683.

--- method.go
+++ method.go
@@ -12,7 +12,7 @@ func BrokenMethod(Data string) bool {
 
 func NumberInputMethod(Data1 bool, Data2 int, Data3 string) int {
     if Data1 {
-        return 1 / 0
+        return 1
     }
 
     if Data2 > 0 {
